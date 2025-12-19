19ba91ce8e6894b726c60cf1fb6b96ac9ec71c34
alokishere <alokvishwakarmabdh@gmail.com>
2025-12-20 01:53:20 +0530

    langchange promptamplate checked

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
 config();
+import {PromptTemplate} from '@langchain/core/prompts';
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 const model = new ChatGoogleGenerativeAI({
@@ -7,6 +8,13 @@ const model = new ChatGoogleGenerativeAI({
     apiKey: process.env.GEMINI_API_KEY,
 });
 
-model.invoke("Hello, what can you do?").then((res) => {
+const template = PromptTemplate.fromTemplate(`
+    explain {topic} in very simple way like ELI5,
+    make sure to inculde the core conscepts and avoide unnecessary jargon,
+    make the answe consis as possible and to the point
+    `)
+
+
+template.pipe(model).invoke({topic:"can you search a dtail about a github id which in 'alokishere' , tell me the detail"}).then(res =>{
     console.log(res.content)
-});
+})
