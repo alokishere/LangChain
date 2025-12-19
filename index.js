1e276d9455a9533609922113a5410f725562adae
alokishere <alokvishwakarmabdh@gmail.com>
2025-12-20 00:04:17 +0530

    chnage made

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { config } from "dotenv";
 config();
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-// const GEMINI_API_KEY = "AIzaSyA2xVtnO9dU6RRI7IF10obasnrazaANkAI";
 
 const model = new ChatGoogleGenerativeAI({
     model: "gemini-2.5-flash",
